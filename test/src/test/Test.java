7ba2c9a63d78144f171624bee1946a0e8f2ff476
jiaoruitao <1105462685@qq.com>
2016-10-21 18:12:06 +0800

    第一次

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -2,6 +2,7 @@ package test;
 
 public class Test {
    public static void main(String[] args) {
+	String str="aaaaaaaaaa";
 	System.out.println("wadewrewrewwrere");
 	System.out.println("wadewrewrewwrere");
 	System.out.println("wadewrewrewwrere");
