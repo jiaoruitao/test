abfb6b2f05d45215cf00747a0448b32a7c387494
jiaoruitao <1105462685@qq.com>
2016-10-21 17:55:17 +0800

    test提交

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,5 +3,8 @@ package test;
 public class Test {
    public static void main(String[] args) {
 	System.out.println("wadewrewrewwrere");
+	System.out.println("wadewrewrewwrere");
+	System.out.println("wadewrewrewwrere");
+	System.out.println("wadewrewrewwrere");
 }
 }
