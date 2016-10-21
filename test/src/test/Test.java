af2d42eb7e6003c959b90e372f988d9291d22633
jiaoruitao <1105462685@qq.com>
2016-10-21 19:05:24 +0800

    第二次

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,11 +1,11 @@
 package test;
 
 public class Test {
-   public static void main(String[] args) {
-	String str="aaaaaaaaaa";
-	System.out.println("wadewrewrewwrere");
-	System.out.println("wadewrewrewwrere");
-	System.out.println("wadewrewrewwrere");
-	System.out.println("wadewrewrewwrere");
-}
+	public static void main(String[] args) {
+		String str="aaaaaaaaaa";
+		System.out.println("wadewrewrewwrere");
+		System.out.println("wadewrewrewwrere");
+		System.out.println("wadewrewrewwrere");
+		System.out.println("wadewrewrewwrere");
+	}
 }
